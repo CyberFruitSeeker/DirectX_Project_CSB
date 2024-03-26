698603de3050af6c944f252492f142e42f2b44f8
CyberFruitSeeker <assortrock0@ASSORTROCK>
2024-03-26 17:00:10 +0900

    프리컴파일, 엔진 종료시 엔진 옵션 세이브 기능 추가

---
+++ DirectX_Project_CSB/EngineCore/PreCompile.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <EnginePlatform/PreCompile.h>
+
+#include <EnginePlatform/EngineSound.h>
+#include <EnginePlatform/EngineWindow.h>
+#include <EnginePlatform/EngineWindow.h>
+#include <EnginePlatform/EngineResources.h>
+#include <EnginePlatform/EngineInput.h>
---
+++ DirectX_Project_CSB/EnginePlatform/PreCompile.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <EngineBase/PreCompile.h>
+#include <EngineBase/EngineTime.h>
+
+#include <EngineBase/EngineMath.h>
+#include <EngineBase/EngineString.h>
+#include <EngineBase/EngineDirectory.h>
+#include <EngineBase/EngineFile.h>
